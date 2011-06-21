549986527c228401e44e435f678a763f3a6abd0c
Patrick Putman <putman.patrick@gmail.com>
2011-06-21 17:08:05 -0500

    Created the Users scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '2b095e6bfd3df2a8a30b84603db87528b5633eae7897e370e2514277918a9995877df0ba57e02bf562eea1943abb092d6e90562817afe07649edfa2c20c845a9'
